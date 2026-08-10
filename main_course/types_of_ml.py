3a69f98dd1b88b0ba12efed58ec28d5a01b5ba19
omshukla0721p <ks0134067@gmail.com>
2026-08-10 11:06:30 +0530

    types of ml

---
+++ main_course/types_of_ml.py
@@ -0,0 +1,43 @@
+# types of ml
+
+# 1) superivised learning
+# we have data which has both input and ouput
+
+# -1.1) regression
+# when we build a model which returns a vaklue mostly an integer
+
+# -1.2) classification
+#  when we build model whcih classifies
+# for eg returning yes or no for detection of cats
+
+
+# 2) Unsupervised learning
+# we have data of input but not output so we build with that 
+# mainly 4 things can be done 
+# -2.1)clustering
+#  grouping the data based on similarities btw differnt inputs
+
+# -2.2) dimensionality reduction
+#  reducing the dimensions of the data in 3d or 2d to visualize it better
+
+# -2.3) anomaly detection
+#  detecting something which is very different from other data points
+
+# -2.4) Association rule learning
+#  just associating things with other 
+# for eg in walmart things we ought to buy together are palced close by 
+
+
+# 3) semi supervised learning
+#  combination of supervised and unsupervised learning
+# 
+# labeling is cotly and timetaking 
+# with this we can label one thing similar things would be labelled automatically
+# for eg naming a person in a phoyograph then automatically all other/
+# photograph in whcih that person is get named automatically
+
+# 4)reiforcement learning
+# we have no Data 
+# we create an agent which gets better overtime 
+# an agent which learns patterns overtime like us
+   
